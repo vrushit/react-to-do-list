54c5a9343cdd1f4d80b14883734d51b52b6a20be
vrushit <bharuchavrushit@ymail.com>
2021-01-25 12:52:57 +0530

    Dummy Tasks

--- Task-tracker/src/components/Task.js
+++ Task-tracker/src/components/Task.js
@@ -1,4 +1,24 @@
-import React from 'react'
+const tasks = [
+
+    {
+        id:1,
+        text:"Doctor Appointment",
+        day: "Feb 5th at 03:00",
+        remainder: true,
+    },
+    {
+        id:2,
+        text:"Workout",
+        day: "Feb 6th at 03:00",
+        remainder: true,
+    },
+    {
+        id:3,
+        text:"Meals",
+        day: "Feb 7th at 03:00",
+        remainder: false,
+    }
+]
 
 const Task = () => {
     return (
