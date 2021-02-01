835116922d0440803c4a16d10ed5a35def4c1da3
vrushit <bharuchavrushit@ymail.com>
2021-02-01 13:23:47 +0530

    Close

--- Task-tracker/src/components/Header.js
+++ Task-tracker/src/components/Header.js
@@ -1,12 +1,12 @@
 import PropTypes from 'prop-types'
 import Button from './Button';
 
-const Header = ({ title, onAdd }) => {
+const Header = ({ title, onAdd, showAdd }) => {
    
     return (
         <header className="header">
             <h1 >{title}</h1>
-            <Button color="steelblue" text ="Add" onClick={onAdd} />
+            <Button color="steelblue" text ={showAdd ? 'Close' :'Add'} onClick={onAdd} />
         </header>
     )
 }
