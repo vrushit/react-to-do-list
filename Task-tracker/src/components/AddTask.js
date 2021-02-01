68198d369c79870a59b1c7c48ac288817fc69514
vrushit <bharuchavrushit@ymail.com>
2021-02-01 12:29:01 +0530

    Adding form

--- Task-tracker/src/components/AddTask.js
+++ Task-tracker/src/components/AddTask.js
@@ -2,9 +2,27 @@ import React from 'react'
 
 const AddTask = () => {
     return (
-        <div>
-            
-        </div>
+        <form className="add-form">
+            <div className="form-control">
+                <label>
+                    Task
+                </label>
+                <input type="text" placeholder="Add Task" />
+            </div>
+            <div className="form-control">
+                <label>
+                    Day & Time
+                </label>
+                <input type="text" placeholder="Add Day and Time" />
+            </div>
+            <div className="form-control">
+                <label>
+                    Set Reminder
+                </label>
+                <input type="checkbox" />
+            </div>
+            <input type="submit" value="Save Task" />
+        </form>
     )
 }
 
